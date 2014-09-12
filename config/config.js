1596f8c310060968725e687bdf23aabb11ca1f29
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-09-12 21:12:14 +0100

    initial commit

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'barcode-monsters-webservice'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/barcode-monsters-webservice-development'
+    
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'barcode-monsters-webservice'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/barcode-monsters-webservice-test'
+    
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'barcode-monsters-webservice'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/barcode-monsters-webservice-production'
+    
+  }
+};
+
+module.exports = config[env];
