506e277d5497b3b8353cbd06b3de106b57cf3893
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-10-31 18:54:50 +0000

    commenting more mongo

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 'use.strict';
 var express = require('express'),
     config = require('./config/config'),
-    glob = require('glob'),
-    mongoose = require('mongoose'),
+    // glob = require('glob'),
+    // mongoose = require('mongoose'),
     coffee = require('coffee-script'),
-    request = require('request')
 
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function() {
-    throw new Error('unable to connect to database at ' + config.db);
-});
+// mongoose.connect(config.db);
+// var db = mongoose.connection;
+// db.on('error', function() {
+//     throw new Error('unable to connect to database at ' + config.db);
+// });
 
-var models = glob.sync(config.root + '/app/models/*.js');
-models.forEach(function(model) {
-    require(model);
-});
+// var models = glob.sync(config.root + '/app/models/*.js');
+// models.forEach(function(model) {
+//     require(model);
+// });
 
 var app = express();
 
 require('./config/express')(app, config);
-console.log(config.port);
+
 app.listen(config.port);
